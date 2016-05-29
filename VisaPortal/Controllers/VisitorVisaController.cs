e22dbd880b651d39d7ed7b97dd21554916f4a308
priyankkansal <priyankkansal@gmail.com>
2016-05-29 10:14:01 -0400

    Added new controller VisitorVisa

---
+++ VisaPortal/Controllers/VisitorVisaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisaPortal.Controllers
+{
+    public class VisitorVisaController : Controller
+    {
+        // GET: VisitorVisa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
