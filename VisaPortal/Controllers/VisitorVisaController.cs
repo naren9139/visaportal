6b1f5fdf9f698ae954cbfe5c618803777d903023
priyankkansal <priyankkansal@gmail.com>
2016-06-01 10:51:46 -0400

    Initial API call

--- VisaPortal/Controllers/VisitorVisaController.cs
+++ VisaPortal/Controllers/VisitorVisaController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Services.Client;
+using gov.data.util;
+using VisaPortal.ServiceDOLAgency;
+using VisaPortal.ServiceDOLSummerJobs;
 
 namespace VisaPortal.Controllers
 {
@@ -11,7 +15,19 @@ namespace VisaPortal.Controllers
         // GET: VisitorVisa
         public ActionResult Index()
         {
-            return View();
+            AgencyEntities entity = new AgencyEntities(new Uri("http://api.dol.gov/V1/DOLAgency"));
+            entity.SendingRequest += new EventHandler<SendingRequestEventArgs>(GOVDataUtil.service_SendingRequest);
+
+            return View(entity.Agencies.ToList<Agencies>());
+
+            //ViewBag.Message = "Welcome to ASP.NET MVC!";
+            //SummerJobsCall proxy = new SummerJobsCall(new Uri("http://api.dol.gov/V1/SummerJobs"));
+            //proxy.SendingRequest += new EventHandler<System.Data.Services.Client.SendingRequestEventArgs>(GOVDataUtil.service_SendingRequest);
+
+            //string jsonData = proxy.Execute<string>(
+            //    new Uri("getJobsListing?format=json&region=&locality=&zip=&employmentType=%27Any%27SkipCount=1&query=%27Nurse%27", UriKind.Relative)
+            //    ).FirstOrDefault().Replace("\\n", "").Replace("\\\"", "\"").Trim('\"');
+
         }
     }
 }
